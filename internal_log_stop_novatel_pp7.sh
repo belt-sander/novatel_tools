909fd9548752bf4d1267aea15e942bbde3209131
belt-sander <sander@mapper.ai>
2019-02-27 12:53:32 -0800

    updated for pp7

---
+++ internal_log_stop_novatel_pp7.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ $# -eq 0 ] 
+	then echo "need to specify port number" 
+	exit
+fi
+
+serialport=/dev/ttyUSB$1
+
+stty -F $serialport 115200 raw -echo   #setup serial port to 115200 on ttyUSB0 (Default)
+exec 3<$serialport 
+	cd /tmp/
+	rm -f ttyDump.dat                    
+	cat <&3 > /tmp/ttyDump.dat & #start serial log
+	PID=$!
+		echo "fileconfig close" > $serialport
+		echo "stopped logging. check that logging LED has stopped blinking!!"
+		sleep 0.5s
+
+		echo "log logfilestatus" > $serialport
+		echo "log status check"
+		sleep 1.0s
+
+		echo "unlogall true" > $serialport
+		echo "removed old logging parameters"
+		sleep 1.0s
+	 kill $PID	
+exec 3<&-
+cat /tmp/ttyDump.dat #show dumped serial output
+
+rm /tmp/ttyDump.dat
+
+echo ""
+echo "check to make sure the green logging LED hath stopped flashing!"
+echo ""
